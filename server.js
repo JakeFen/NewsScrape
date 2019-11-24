93142186082bb6542d90d55b6043d1baf8ed86d0
Jacob Fenwick <jacobfenwick111@gmail.com>
2019-11-24 00:52:11 -0500

    ..

--- server.js
+++ server.js
@@ -133,6 +133,6 @@ app.get("/article/:id", function(req, res) {
 });
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://mongoMongoose:rootpass1@ds117681.mlab.com:17681/heroku_xfz8l3b7", {
+mongoose.connect("mongodb://localhost/mongoMongoose", {
   useNewUrlParser: true
 });
